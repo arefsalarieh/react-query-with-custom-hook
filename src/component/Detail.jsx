a66a402cf7c2171ecf93c1715eeb4472c75f1eb3
aref <arefsalarieh@gmail.com>
2024-10-29 23:35:31 +0400

    update detail

--- src/component/Detail.jsx
+++ src/component/Detail.jsx
@@ -3,16 +3,19 @@ import { useParams } from 'react-router-dom'
 import { useMutation, useQueryClient } from 'react-query'
 import { Field, Formik ,Form } from 'formik'
 import axios from 'axios'
-import CustomPutUseMutation from '../customHook/CustomPutUseMutation'
 import useQueryGet from '../customHook/useQueryGet'
 import detailPic from '../assets/images/detailPic.webp'
+import EditDetail from './EditDetail'
 
 
 const Detail = () => {
     const {id} = useParams()
-    console.log(id);
+
      const {data , status , refetch} = useQueryGet('detail' , `https://66e301e5494df9a478e3f4f6.mockapi.io/test/test/${id}` )
-    data && console.log(data);
+
+
+
+    
 
 
 
@@ -79,29 +82,43 @@ const Detail = () => {
 
     // })
 
-    const {mutate} = CustomPutUseMutation()
 
-
-    const handleMutate = (values)=>{
-        let obj = {url:`https://662652ab052332d553227616.mockapi.io/test/test/${id}`, values:values}
-        mutate(obj)
-    }
 
 
 
   return (
     <div>
-        <div className='w-[80%] flex bg-[#F0F0F0] h-96 mx-auto mt-10'>
+        <div className='w-[80%] flex bg-[#F0F0F0]  mx-auto mt-0'>
              <div className='w-1/2 flex flex-col gap-6 mt-10 px-6'>
-                <h2>course name : {data && data.courseOrBlogName}</h2>
-                <h2 className='h-36'>describe : {data && data.describe}</h2>
-                <h2 >tech : {data && data.tech}</h2>
-                <h2 >cost : {data && data.cost}</h2>
+
+                <div className='flex gap-5'>
+                  <h2 className='bg-[aqua] p-3 rounded-xl'>course name :</h2>
+                  <h4 className='p-3'> {data && data.courseOrBlogName}</h4>
+                </div>
+
+                <div className='flex gap-5 '>
+                  <h2 className='bg-[aqua] p-3 rounded-xl'>describe :</h2>
+                  <h4 className='p-3'> {data && data.describe}</h4>
+                </div>
+
+                <div className='flex gap-5'>
+                  <h2 className='bg-[aqua] p-3 rounded-xl'>tech :</h2>
+                  <h4 className='p-3'>  {data && data.tech}</h4>
+                </div>        
+
+                <div className='flex gap-5'>
+                  <h2 className='bg-[aqua] p-3 rounded-xl'>cost :</h2>
+                  <h4 className='p-3'> {data && data.cost}</h4>
+                </div>            
+
+                {data && <EditDetail data={data} id={id}/>}
              </div>
              <div className='w-1/2'>
-                <img src={detailPic} alt="" />
+                <img src={detailPic} className='' alt="" />
              </div>
         </div>
+
+        
     </div>
   )
 }
